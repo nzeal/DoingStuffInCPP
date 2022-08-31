608652e08b36acbdf7c8cbf10b5c0c0cbabaa18d
Nitin Shukla <n.shukla@cineca.it>
2022-08-31 12:33:36 +0200

    swap.cpp

---
+++ swaptwoInt/swap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void Swap(T& a, T& b)
+{
+  T tmp = a;
+  a = b;
+  b = tmp;
+};
+
+template <typename T>
+void myPrint(T& a, T& b)
+{
+  cout << "a = " << a  << "; " << "a = " << b << "; " << endl;
+}
+
+int main()
+{
+  int a = 2, b = 10; 
+  cout << "Before passing data to function template.\n";
+  myPrint(a,b);
+  Swap(a,b);
+  cout << "\nAfter passing data to function template.\n";
+  myPrint(a,b);
+  
+  return 0;
+}
